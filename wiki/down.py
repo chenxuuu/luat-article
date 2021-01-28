0be5cbdf3dddaa3728d43d8dd5eb711cb9cef6cf
chenxuuu <lolicon@papapoi.com>
2021-01-28 17:48:59 +0800

    修正不规范md标题，更改wiki内链

--- wiki/down.py
+++ wiki/down.py
@@ -15,21 +15,33 @@ opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 10.0; Win64; rv:60.0) 
 urllib.request.install_opener(opener)
 
 def down(info):
-    id = str(info["id"])
-    url = "http://doc.openluat.com/api/site/text?id="+str(info["text_id"])
-    html = urllib.request.urlopen(url,timeout=30).read().decode('utf-8')
-    j = json.loads(html)
-    text = j["data"]["content"]
-    text = re.sub('http://doc.openluat.com/article/(.+?)/0',
-        lambda x: "https://doc.luatos.wiki/{}/".format(x.group(1)),
-        text)
-    md = open("pages/"+id+".md", "w",encoding='utf-8')
-    md.write("# "+info["title"]+"\n\n")
-    md.write(text)
-    md.close()
-    os.system("pandoc -o pages/"+id+".rst pages/"+id+".md")
-    os.remove("pages/"+id+".md")
-    print("download done "+str(id))
+    while True:
+        try:
+            id = str(info["id"])
+            url = "http://doc.openluat.com/api/site/text?id="+str(info["text_id"])
+            html = urllib.request.urlopen(url,timeout=30).read().decode('utf-8')
+            j = json.loads(html)
+            text = "\n"+j["data"]["content"]
+            text = text.replace("\n# ","\n## ")
+            text = re.sub('http://doc.openluat.com/article/(\\d+)/0',
+                lambda x: "https://doc.luatos.wiki/{}/".format(x.group(1)),
+                text)
+            text = re.sub('http://doc.openluat.com/wiki/6\\?wiki_page_id=(\\d+)',
+                lambda x: "https://doc.luatos.wiki/wiki/pages/{}.html".format(x.group(1)),
+                text)
+            text = re.sub('\n(# +.+)',
+                lambda x: "\n\n{}\n".format(x.group(1)),
+                text)
+            md = open("pages/"+id+".md", "w",encoding='utf-8')
+            md.write("# "+info["title"].replace("-"," ").replace("#"," ")+"\n\n")
+            md.write(text)
+            md.close()
+            os.system("pandoc -o pages/"+id+".rst pages/"+id+".md")
+            os.remove("pages/"+id+".md")
+            print("download done "+str(id))
+            break
+        except:
+            pass
 
 url = "http://doc.openluat.com/api/site/wiki_page?wiki_id=6"
 html = urllib.request.urlopen(url,timeout=30).read().decode('utf-8')
